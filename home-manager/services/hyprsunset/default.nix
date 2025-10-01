747d2bc729c8e03ccb368b3343d98cedc1170293
nielsing <niels.ingi@gmail.com>
2025-10-01 22:49:21 +0000

    Update

--- home-manager/services/hyprsunset/default.nix
+++ home-manager/services/hyprsunset/default.nix
@@ -17,21 +17,5 @@
         }
       ];
     };
-    transitions = {
-      sunrise = {
-        calendar = "*-*-* 06:00:00";
-        requests = [
-          ["temperature" "6500"]
-          ["gamma 100"]
-        ];
-      };
-      sunset = {
-        calendar = "*-*-* 21:30:00";
-        requests = [
-          ["temperature" "3500"]
-          ["gamma 50"]
-        ];
-      };
-    };
   };
 }
